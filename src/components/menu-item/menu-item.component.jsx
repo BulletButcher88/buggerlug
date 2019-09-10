213ff6c5be26e41b86162b89e1d834cbcfb3d678
Bull-Butcher <markjamesbutcher@gmail.com>
2019-09-10 14:36:07 +1000

    scss added to image

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import './menu-item.style.scss';
 
 const MenuItem = ({ title, imageUrl, size}) => (
-  <div style={{  
-    backgroundImage: `url(${imageUrl})`
-}}
+  <div 
   className={`${size} menu-item`} >
+
+    <div className='background-image'
+    style={{  
+      backgroundImage: `url(${imageUrl})`
+      }}/>
+      
   <div className="content">
-    <h1 className='title'>{title}</h1>
+    <h1 className='title'>{ title.toUpperCase() }</h1>
     <spam className="subtitle">SHOP NOW</spam>
   </div>
 </div>
