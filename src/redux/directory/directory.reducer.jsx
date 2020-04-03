3150809f112bd935b59090fdd0457ec6f0fcb540
Mark Butcher <markjamesbutcher@gmail.com>
2020-04-03 14:35:12 +1100

    test images for Kayo and Good Guys Never Win toys

--- src/redux/directory/directory.reducer.jsx
+++ src/redux/directory/directory.reducer.jsx
@@ -19,18 +19,20 @@ const INITIAL_STATE = {
       linkUrl: "shop/sneakers"
     },
     {
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      title: "Good Guys Never Win",
+      imageUrl:
+        "https://4.bp.blogspot.com/-kqon34ra7-8/XWOPllVDNdI/AAAAAAAC_-0/FnfmtSiBq6g-lqbxM-4h9LW7wKEqg9y8gCLcBGAs/s640/Good%2BGuys%2BNever%2BWin%2BToys%2BCo%2Bfor%2B%2523ThisIsNotAToyScene%2BExhibition%2B%2540%2BBSides%2BGallery%2B%2528Opens%2BAug%2B28%2529.jpg",
       size: "large",
       id: 4,
-      linkUrl: "shop/womens"
+      linkUrl: "shop/goodguy"
     },
     {
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      title: "kayo",
+      imageUrl:
+        "https://theaither.com/wp-content/uploads/2019/10/D5859719-0F79-42C3-AD5E-29ED6822EAF1-copy.jpg",
       size: "large",
       id: 5,
-      linkUrl: "shop/mens"
+      linkUrl: "shop/kayo"
     }
   ]
 };
