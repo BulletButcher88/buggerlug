6f70e6f5a1cd518389196f0a069b0e7742c5d6ca
Mark Butcher <markjamesbutcher@gmail.com>
2020-04-06 14:19:51 +1000

    collection-container refactoring

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.action";
-import {
-  selectIsCollectionFetching,
-  selectIsCollectionsLoaded,
-} from "../../redux/shop/shop.selectors";
 
+import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 import CollectionsOverviewContainer from "../../components/collection-overview/collection-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
 
 import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
-import CollectionPage from "../collection/collection.component";
-import WithSpinner from "../../components/with-spinner/with-spinner";
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -25,7 +17,7 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionsLoaded } = this.props;
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route
@@ -35,23 +27,15 @@ class ShopPage extends React.Component {
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={(props) => (
-            <CollectionPageWithSpinner
-              isLoading={!isCollectionsLoaded}
-              {...props}
-            />
-          )}
+          component={CollectionPageContainer}
         />
       </div>
     );
   }
 }
-const mapStateToProps = createStructuredSelector({
-  isCollectionsLoaded: selectIsCollectionsLoaded,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/directory/directory.reducer.jsx
+++ src/redux/directory/directory.reducer.jsx
@@ -1,25 +1,24 @@
 const INITIAL_STATE = {
   sections: [
     {
-      title: "heartlad",
-      imageUrl:
-        "https://1.bp.blogspot.com/-wPkAgdzlvpQ/XoLh6QmtqWI/AAAAAAADNuU/mukV8IX-4CM3_sp8XSDRqaZaqBLs3XUCQCLcBGAsYHQ/w1320/%2522EPIC%2BHADOUKEN%2522%2Bby%2BHANH%2Bfor%2BHEARTLAB%2Bon%2BTOYSREVIL%2B01.jpg",
+      title: "trash cult",
+      imageUrl: "https://i.imgur.com/TTB9Moa.jpg",
       id: 1,
-      linkUrl: "shop/heartlad"
+      linkUrl: "shop/trashcult",
     },
     {
       title: "abiebi",
       imageUrl:
         "https://4.bp.blogspot.com/-h7e4Mg-02dE/XoIovYL73yI/AAAAAAADNtE/tiljemmtP88Cx9AoK2MWUzvQoix9COtRACLcBGAsYHQ/w1320/Dadballs%2B%2522Love%2BYour%2BSelf%2522%2Bby%2BABIEBI%2B1.jpeg",
       id: 2,
-      linkUrl: "shop/abiebi"
+      linkUrl: "shop/abiebi",
     },
     {
       title: "rare collectables",
       imageUrl:
         "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F07%2Fbest-art-for-sale-online-july-2019-week-3-01.jpg?quality=95&w=1170&cbr=1&q=90&fit=max",
       id: 3,
-      linkUrl: "shop/rare"
+      linkUrl: "shop/rare",
     },
     {
       title: "Good Guys Never Win",
@@ -27,7 +26,7 @@ const INITIAL_STATE = {
         "https://4.bp.blogspot.com/-kqon34ra7-8/XWOPllVDNdI/AAAAAAAC_-0/FnfmtSiBq6g-lqbxM-4h9LW7wKEqg9y8gCLcBGAs/s640/Good%2BGuys%2BNever%2BWin%2BToys%2BCo%2Bfor%2B%2523ThisIsNotAToyScene%2BExhibition%2B%2540%2BBSides%2BGallery%2B%2528Opens%2BAug%2B28%2529.jpg",
       size: "large",
       id: 4,
-      linkUrl: "shop/goodguy"
+      linkUrl: "shop/goodguy",
     },
     {
       title: "kayo",
@@ -35,9 +34,9 @@ const INITIAL_STATE = {
         "https://theaither.com/wp-content/uploads/2019/10/D5859719-0F79-42C3-AD5E-29ED6822EAF1-copy.jpg",
       size: "large",
       id: 5,
-      linkUrl: "shop/kayo"
-    }
-  ]
+      linkUrl: "shop/kayo",
+    },
+  ],
 };
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
